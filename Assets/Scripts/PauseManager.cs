d439d442bc0773c315737653ac1086192add7c0c
Padi66 <ar.padawan@gmail.com>
2026-03-01 12:18:19 +0100

    [ARO]Feedback

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+    private bool isPaused = false;
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+            pausePanel.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            pausePanel.SetActive(false);
+        }
+    }
+}
